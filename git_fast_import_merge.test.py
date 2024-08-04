5309bea75c85dc6ee0965279e23e3ec82adf28a6
Guido Draheim <guidod@gmx.de>
2024-08-04 13:44:14 +0200

    +info

--- git_fast_import_merge.test.py
+++ git_fast_import_merge.test.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python3
 
+__copyright__ = "(C) 2023-2024 Guido Draheim, licensed under the Apache License 2.0"""
+__version__ = "1.0.1317"
+
 from unittest import main, TestCase
 import git_fast_import_merge as app
 
